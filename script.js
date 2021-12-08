14b6677da1d5775dd13ba14bd3e6658f5e8098ac
AmandineCT <95756738+AmandineCT@users.noreply.github.com>
2021-12-08 15:24:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const navbar = document.querySelector('#navigation');
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 50) {
+        navbar.classList.add('scroll');
+    } else {
+        navbar.classList.remove('scroll');
+    }
+});

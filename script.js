32fe3b66f3cac256ce32c20a423fd3b9f04d3aad
AmandineCT <95756738+AmandineCT@users.noreply.github.com>
2021-12-10 12:29:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//navbar
+
 const navbar = document.querySelector('#navigation');
 window.addEventListener('scroll', () => {
     if(window.scrollY > 50) {
@@ -5,4 +7,21 @@ window.addEventListener('scroll', () => {
     } else {
         navbar.classList.remove('scroll');
     }
+});
+
+//popups
+
+const popupContainer = document.querySelector('#popup-container');
+const popup = popupContainer.querySelector('#popup');
+const titrePopup = popupContainer.querySelector("#popupTitle");
+const popupParts =  popupContainer.querySelectorAll('.popupPart');
+let closeTop = document.querySelector('#closeTop');
+let closeBottom = document.querySelector('#closeBottom');
+
+closeTop.addEventListener('click', () => {
+    popupContainer.classList.remove('active');
+});
+
+closeBottom.addEventListener('click', () => {
+    popupContainer.classList.remove('active');
 });

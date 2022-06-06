a5288e1ec32c7e3b50ea5eea71ddac721f9797f3
AmandineCT <atribot.ing2023@esaip.org>
2022-06-06 14:32:19 +0200

    safe

--- babel-0.4.1/babel/redirect.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(0);
-if(!isset($_GET["newurl"]) ){
-	header("location: index.php");
-}else{
-	if(isset($_GET["newlang"]))	setcookie('usrlang',$_GET["newlang"],time()+86400*30,'/');
-	header("location: ".$_GET["newurl"]);
-}
-
